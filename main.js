f22977d292c025ae1a91a93019d6dfb0f948627f
“Lauren <lucerolaurenm@gmail.com>
2020-02-05 17:45:37 -0700

    Style input text, require input and numbers only

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("JavaScript test");
+
+minutesInput = document.querySelector('.minutes');
+
+secondsInput = document.querySelector('.seconds');
+
+minutesInput.addEventListener('input', restrictNumber);
+function restrictNumber (e) {
+  var newValue = this.value.replace();
+  this.value = newValue;
+}
+
+secondsInput.addEventListener('input', restrictNumber);
+function restrictNumber (e) {
+  var newValue = this.value.replace();
+  this.value = newValue;
+}
