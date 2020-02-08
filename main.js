4b258d324fe7f081959bec131f051096fbe2f25f
Carlyle Douglas <cdouglascorp@gmail.com>
2020-02-08 11:11:15 -0700

    Added button select color changing functionality

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("JavaScript test");
+// console.log("JavaScript test");
+var meditate = document.querySelector('.meditate-btn');
+var exercise = document.querySelector('.exercise-btn');
+var study = document.querySelector('.study-btn');
+var medActive = document.querySelector('#meditate-btn');
+var studyActive = document.querySelector('#study-btn');
+var exerciseActive = document.querySelector('#exercise-btn');
+
+
+meditate.addEventListener('click', changeBtnColor)
+exercise.addEventListener('click', changeBtnColor)
+study.addEventListener('click', changeBtnColor)
+
+function changeBtnColor() {
+  meditate.classList.add('med-btn-selected');
+  exercise.classList.add('exercise-btn-selected');
+  study.classList.add('study-btn-selected');
+  medActive.src = "assets/meditate-active.svg";
+  studyActive.src = "assets/study-active.svg";
+  exerciseActive.src = "assets/exercise-active.svg";
+}
